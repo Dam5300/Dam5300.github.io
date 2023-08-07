150d9d59e1f0e7d7fab1bdeb02395d85168c84d3
Dam5300 <damiano.marconato@unipd.it>
2023-08-07 16:34:29 +0200

    meglio snello e leggero

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("menu-button").addEventListener("click", function() {
-    var menu = document.getElementById("menu");
-    menu.style.display = menu.style.display === "none" ? "block" : "none";
-  });
-  
