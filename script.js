eaeecb7ce2c532d5bcd73e02a3a304f4f892a8a6
Dam5300 <damiano.marconato@unipd.it>
2023-08-07 16:20:10 +0200

    Prova menu pulsanti tondi

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("menu-button").addEventListener("click", function() {
+    var menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "none" ? "block" : "none";
+  });
+  
